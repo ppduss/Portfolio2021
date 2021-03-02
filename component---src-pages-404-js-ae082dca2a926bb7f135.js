926951f872818f37abba18c9d5a6099b52ad9e9c
Patrick <ppduss@gmail.com>
2021-03-01 21:16:41 -0500

    Updates

---
+++ component---src-pages-404-js-ae082dca2a926bb7f135.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),l=n("vrFN");e.default=function(){return u.a.createElement("div",null,u.a.createElement(l.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... :(."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ae082dca2a926bb7f135.js.map
